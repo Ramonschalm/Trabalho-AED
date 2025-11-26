bdde8822fb5a7f18b3da1ccd113bf80eb532f007
Paloma Jasminie <palomajasminie@outlook.com>
2025-11-25 21:04:42 -0300

    Adicionando classe carta do jogo

---
+++ Trabalho_AED/Trabalho_AED/Carta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_AED
+{
+    internal class Carta
+    {
+        int numero;
+        string naipe;
+
+        public Carta(int numero, string naipe)
+        {
+            this.numero = numero;
+            this.naipe = naipe;
+        }
+    }
+}
